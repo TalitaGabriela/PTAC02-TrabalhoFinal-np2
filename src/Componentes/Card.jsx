f2ca95bb648a84654699a8ebc7cfa9adab331c4c
TalitaGabriela <talitagabriela1216@gmail.com>
2023-11-24 14:32:55 -0400

    Commit
    Implementando arquivos do Trabalho Final partindo da explicação do professor.

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,7 +1,24 @@
-export default function Card({atividade}){
+import { Link } from "react-router-dom";
+import '../styles/card.css'
+
+export default function Card({listaTrailers}){
 return(
-<div>
-    <h1>{atividade.atividade}</h1>
-</div>
+    listaTrailers.map((video) =>
+    <div className='card'>
+        <iframe>
+            width = "853"
+            height = "480"
+            src = {
+                'https://youtu.be/Qieu0vtPaKc?si=u6d0kcMi7uzrQ4QZ' + video.url.slice(17)}
+            frameborder = "0"
+            allow = "accelerometer; autoplay; clipboard-write ; encrypted-media"   
+        </iframe>
+        <Link to ={'/detalhe/$video.id'}>
+            <span>
+                {video.nome}
+            </span>
+        </Link>
+    </div>
+    )
 );
 }
