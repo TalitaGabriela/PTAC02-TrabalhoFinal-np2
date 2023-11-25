70c810a8f3f6521df7c712dc5aef2ec5a69e12ec
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-11-25 18:11:33 -0400

    Commit
    Correção da Frame do vídeo na função Card

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -5,15 +5,15 @@ export default function Card({listaTrailers}){
 return(
     listaTrailers.map((video) =>
     <div className='card'>
-        <iframe>
+        <iframe
             width = "853"
             height = "480"
             src = {
                 'https://youtu.be/Qieu0vtPaKc?si=u6d0kcMi7uzrQ4QZ' + video.url.slice(17)}
             frameborder = "0"
-            allow = "accelerometer; autoplay; clipboard-write ; encrypted-media"   
+            allow = "accelerometer; autoplay; clipboard-write ; encrypted-media gyroscope; picture-in-picture" allowfullscreen>   
         </iframe>
-        
+
         <Link to ={'/detalhe/$video.id'}>
             <span>
                 {video.nome}
